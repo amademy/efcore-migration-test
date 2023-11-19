8c0e07658e9c67bae34da4084f59e58428afcbf9
Arad Alvand <aradalvand@gmail.com>
2023-11-19 18:30:30 +0330

    Initial

---
+++ Migrations/20231119145810_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace efcore_migration_test.Migrations
+{
+	/// <inheritdoc />
+	public partial class Initial : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterDatabase()
+				.Annotation("Npgsql:Enum:product_trait", "foo,bar,buzz");
+
+			migrationBuilder.CreateTable(
+				name: "Products",
+				columns: table => new
+				{
+					Id = table.Column<int>(type: "integer", nullable: false)
+						.Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+					Traits = table.Column<List<ProductTrait>>(type: "product_trait[]", nullable: false)
+				},
+				constraints: table =>
+				{
+					table.PrimaryKey("PK_Products", x => x.Id);
+				});
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropTable(
+				name: "Products");
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
+using var db = new AppDbContext();
+var products = db.Products.ToList();
+
+class AppDbContext : DbContext
+{
+	static AppDbContext()
+	{
+#pragma warning disable
+		NpgsqlConnection.GlobalTypeMapper
+			.MapEnum<ProductTrait>();
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder
+			.HasPostgresEnum<ProductTrait>();
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+		optionsBuilder
+			.UseNpgsql(new NpgsqlConnectionStringBuilder
+			{
+				Host = "localhost",
+				Username = "arad",
+				Password = "1301381",
+				Database = "ef_migrations_test_db",
+			}.ToString())
+			.LogTo(Console.WriteLine);
+
+	public DbSet<Product> Products => Set<Product>();
+}
+
+class Product
+{
+	public required int Id { get; set; }
+	public required List<ProductTrait> Traits { get; set; }
+}
+
+enum ProductTrait
+{
+	Foo,
+	Bar,
+	Buzz,
+}
